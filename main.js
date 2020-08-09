15a03d4baa31a4a91c94de3aea1d1873eb361f27
Julian Chajdys <jchajdys@gmail.com>
2020-08-09 17:31:26 +0200

    Added node_modules/ and package-lock.json

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
+});
+
 client.on('ready', () => {
     console.log(`Zalogowano jako: ${client.user.tag}!`);
   });
