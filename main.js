405d3db7608903cfc0e78b021b4d77407bb73668
Julian Chajdys <jchajdys@gmail.com>
2020-08-09 19:07:00 +0200

    Added GIF command and http library

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const http = require('http');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
@@ -14,7 +15,30 @@ client.on('message', message => {
 	} else if (command === 'beep') {
 		message.channel.send('Boop.');
 	}
-	// other commands...
+	else if (command === "gif"){
+    var url = 'http://api.giphy.com/v1/gifs/random?api_key=yPU9btxDmzgEk8ZXhmO3gaOcROfROZH0&tag=meme';
+
+http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var giphyresponse = JSON.parse(body);
+        const gifembed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('GIF')
+        .setImage(giphyresponse.data.image_url)
+        .setTimestamp()
+        .setFooter("Komenda wywołana przez: " + message.author.tag, message.author.displayAvatarURL())
+        message.channel.send(gifembed);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
+  }
 });
 
 client.on('ready', () => {
