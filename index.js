e6ebb8f6e14db70d0b57ce700cdede373cbc30c4
Julian Chajdys <jchajdys@gmail.com>
2020-10-07 17:41:48 +0200

    Fixed all my shity bugs :3

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
 const Discord = require('discord.js');
 const http = require('http');
+const https = require('https');
 const axios = require('axios');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
-const fs = require('fs');
-
-let date_ob = new Date();
-let date = ("0" + date_ob.getDate()).slice(-2);
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-let year = date_ob.getFullYear();
-let hours = date_ob.getHours();
-let minutes = date_ob.getMinutes();
-let seconds = date_ob.getSeconds();
-let ts = `${hours}:${minutes}:${seconds} ${month}/${date}/${year}`;
-
-client.on('message', async message => { 
-  
-if (message.mentions.has(client.user)) {
-  if (message.content.has("@here")) return;
-  if (message.content.has("@everyone")) return;
-  fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: @mention, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-    if (err) throw err;
-
-})
-  const embed = new Discord.MessageEmbed()
-  .setTitle("Informacje o bocie")
-  .addField("Prefix bota:", prefix)
-  .addField("Dostępne komendy:", `${prefix}help`)
-  .addField("Serwery:", client.guilds.cache.size)
-  .setFooter("Komenda wywołana przez: " + message.author.tag, message.author.displayAvatarURL())
-  .setTimestamp()
-  message.channel.send(embed)
-}
-});
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+  const command = args.shift().toLowerCase();
   if(command === "gif"){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: GIF, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     var url = 'http://api.giphy.com/v1/gifs/random?api_key=yPU9btxDmzgEk8ZXhmO3gaOcROfROZH0&tag=meme';
 
 http.get(url, function(res){
@@ -67,10 +34,6 @@ http.get(url, function(res){
 });
   }
   else if(command === "help"){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: HELP, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
   const helpembed = new Discord.MessageEmbed()
     .setTitle("Lista komend:")
     .setDescription("**:laughing: Zabawne:** \n `$gif`, `$polishmeme` , `$meme` \n **:frame_photo: Obrazy:**\n`$supreme [tekst]`, `$captcha [tekst]`, `$pies`, `$kot`, `$borsuk` \n<:reddit:742766045340631070> Reddit: \n `$randompost [subreddit]`, `$dank`, `$pewdiepie`, `$eyebleach` \n **:tools: Moderacyjne:**\n `$ban [@użytkownik]`, `$kick [@użytkownik]` \n **:information_source: Informacyjne:**\n `$help`, `$ping`, `$invite`")
@@ -82,10 +45,6 @@ http.get(url, function(res){
   message.channel.send(helpembed);
   }
   else if(command === "polishmeme"){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: POLISHMEME, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
   var url = 'https://ivall.pl/memy';
 
 http.get(url, function(res){
@@ -110,10 +69,6 @@ http.get(url, function(res){
 });
   }
   else if(command === "meme"){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: MEME, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
   var url = 'http://meme-api.herokuapp.com/gimme/memes';
 
 http.get(url, function(res){
@@ -152,10 +107,6 @@ http.get(url, function(res){
 });
   }
   else if(command === `ping`) {
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: PING, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
          var embed = new Discord.MessageEmbed()
         .setAuthor(`Obliczanie pingu...`)
         .setColor("111")
@@ -168,10 +119,7 @@ http.get(url, function(res){
         });
   }
   else if(command === `borsuk`){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: BORSUK, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
+   
     try{
       const res = await axios({
           url: 'https://multiapp.xyz/api/images/badgers',
@@ -191,10 +139,6 @@ http.get(url, function(res){
   }
   else if(command === `supreme`){
     const text = message.content.replace(prefix + "supreme ", "" ,)
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: SUPREME (TEXT: ${text}), Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     const link = "https://api.alexflipnote.dev/supreme?text=" + text.replace(/ /g , "+" )
     const embed = new Discord.MessageEmbed()
     .setTitle("Supreme")
@@ -204,10 +148,6 @@ http.get(url, function(res){
   }
   else if(command === `captcha`){
     const text = message.content.replace(prefix + "captcha " , "")
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: CAPTCHA (Text: ${text}), Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     const link = "https://api.alexflipnote.dev/captcha?text=" + text.replace(/ /g, "+" )
     const embed = new Discord.MessageEmbed()
     .setTitle("Captcha")
@@ -225,10 +165,6 @@ http.get(url, function(res){
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
-      fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: KICK (Member: ${user.tag} ID: ${user}), Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-        if (err) throw err;
-    
-    })
       if (member) {
         member
           .kick("Wyrzucono przez" + message.author.tag + " (ID: " + message.author + " )")
@@ -278,10 +214,6 @@ http.get(url, function(res){
     .setFooter("Komenda wywołana przez: " + message.author.tag , message.author.displayAvatarURL())
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(embed)
     const user = message.mentions.users.first();
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: BAN (Member: ${user.tag} ID: ${user}), Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     if (user) {
       const member = message.guild.member(user);
       if (member) {
@@ -326,10 +258,6 @@ http.get(url, function(res){
     }
   }
   else if(command === `invite`){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: INVITE, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     const embed = new Discord.MessageEmbed()
     .setTitle("Linki")
     .addField(":link: Dodaj bota", " [Kliknij tutaj](https://discord.com/oauth2/authorize?client_id=686128717826752532&permissions=8&scope=bot) aby dodać bota na serwer")
@@ -340,10 +268,6 @@ http.get(url, function(res){
   }
   else if(command === `randompost`){
     const subreddit = message.content.replace(prefix + "randompost ", "" ,)
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: RANDOMPOST (/r/${subreddit}/), Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
   if (subreddit = ""){
     const embed = new Discord.MessageEmbed()
     .setColor('DARK_RED')
@@ -389,10 +313,6 @@ http.get(url, function(res){
     });
   }
   else if(command === `dank`){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: DANKMEME, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     var url = 'http://meme-api.herokuapp.com/gimme/dankmemes';
 
     http.get(url, function(res){
@@ -428,10 +348,6 @@ http.get(url, function(res){
     });
   }
   else if(command === `pewdiepie`){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: PEWDIEPIE, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     var url = 'http://meme-api.herokuapp.com/gimme/pewdiepiesubmissions';
 
     http.get(url, function(res){
@@ -467,10 +383,6 @@ http.get(url, function(res){
     });
   }
   else if(command === `eyebleach`){
-    fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: EYEBLEACH, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-      if (err) throw err;
-  
-  })
     var url = 'http://meme-api.herokuapp.com/gimme/eyebleach';
 
     http.get(url, function(res){
@@ -507,11 +419,7 @@ http.get(url, function(res){
  
  }
   else if(command === `kot`){
-  fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: KOT, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-    if (err) throw err;
-
-})
-  var url = 'http://api.alexflipnote.dev/cats';
+  var url = 'http://aws.random.cat/meow';
 
   http.get(url, function(res){
     var body = '';
@@ -521,7 +429,7 @@ http.get(url, function(res){
     });
   
     res.on('end', function(){
-        var memresponse = JSON.parse();
+        var memresponse = JSON.parse(body);
         const memembed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Kot')
@@ -535,25 +443,22 @@ http.get(url, function(res){
   });
 }
 else if(command === `pies`){
-  fs.appendFile('/home/julek/funbot-logs/command.log', `\n ${ts} Command: PIES, Author: ${message.author.tag}(ID: ${message.author}), Guild: ${message.guild.name} (ID: ${message.guild.id}), Channel: ${message.channel.id}, Message: ${message.id}`, (err) => {
-    if (err) throw err;
+  var url = 'https://dog.ceo/api/breeds/image/random';
 
-})
-  var url = 'http://api.alexflipnote.dev/dogs';
-
-  http.get(url, function(res){
+  https.get(url, function(res){
     var body = '';
   
     res.on('data', function(chunk){
         body += chunk;
+    
     });
   
     res.on('end', function(){
-        var memresponse = JSON.parse();
+        var memresponse = JSON.parse(body);
         const memembed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Pies')
-        .setImage(memresponse.file)
+        .setImage(memresponse.message)
         .setTimestamp()
         .setFooter("Komenda wywołana przez: " + message.author.tag, message.author.displayAvatarURL())
         message.channel.send(memembed);
@@ -568,10 +473,6 @@ else if(command === `pies`){
 
 client.on('ready', () => {
     console.log(`Zalogowano jako: ${client.user.tag}!`);
-    fs.appendFile('/home/julek/funbot-logs/boot.log', `\n ${ts} Bot został włączony!`, (err) => {
-      if (err) throw err;
-  
-  })
     client.user.setActivity(`$help | Serwery: ${client.guilds.cache.size}`), {type: 'LISTENING'};
   });
   client.on("guildCreate", guild => {
@@ -586,11 +487,6 @@ client.on('ready', () => {
   }).then(invite => {
     console.log(`Dołączyłem do serwera: ${guild.name} (id: ${guild.id}). Ten serwer ma ${guild.memberCount} członków! Zaproszenie: https://discord.gg/${invite.code}`);
     client.user.setActivity(`@funbot.js | Serwery: ${client.guilds.cache.size}`), {type: 'LISTENING'};
-    fs.appendFile('/home/julek/funbot-logs/server.log', `\n${ts}Dołączyłem do serwera: ${guild.name} (id: ${guild.id}). Ten serwer ma ${guild.memberCount} członków! Zaproszenie: https://discord.gg/${invite.code}`, (err) => {
-      if (err) throw err;
-  
   })
-  })
-  });
-
+});
 client.login(token);
